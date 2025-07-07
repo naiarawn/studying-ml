9dc94914e16e56448739d6019ae749de4c8328b2
naiarawn <n4iara@gmail.com>
2025-07-07 18:15:34 -0300

    project star wars: decision tree classifier - features changed and max depth added

--- starwars.py
+++ starwars.py
@@ -3,14 +3,15 @@ import pandas as pd
 
 df = pd.read_parquet('data/dados_clones.parquet'); df
 # %%
-# features = ["Massa(em kilos)", "General Jedi encarregado",	"Estatura(cm)", "Distância Ombro a ombro",	"Tamanho do crânio",	"Tamanho dos pés",	"Tempo de existência(em meses)"]
 features = ["Massa(em kilos)", "Estatura(cm)"]
 target = 'Status '
 # %%
 X = df[features]
 y = df[target]
 # %%
-#categorical to numerical
+# categorical to numerical
+# no need to convert because the data used is already numerical
+# if it were categorical, we would use the following code:
 dic_replace = {
   'Tipo 1': 1, 'Tipo 2': 2,
   'Tipo 3': 3, 'Tipo 4': 4,
@@ -31,4 +32,5 @@ plt.figure(dpi=400)
 tree.plot_tree(model,
                 feature_names=features,
                 class_names=model.classes_,
-                filled=True)
+                filled=True,
+                max_depth=3) # 3 is the maximum depth of the tree
